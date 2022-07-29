bb299996adee2b9c906941a90f3fc5817d981c01
pathymavuba <mavubapathy@gmail.com>
2022-07-29 15:23:45 +0100

    supprimer les tâches

--- src/App.js
+++ src/App.js
@@ -24,11 +24,15 @@ function App() {
         },
     ]
 )
-  
+ //delete Task
+const deleteTask = (id)=>{
+  setTasks(tasks.filter((task)=>task.id !== id))
+}
+ 
 return (
     <div className="container">
         <Header  />
-        <Tasks tasks={tasks}/>
+        {tasks.length>0?(<Tasks tasks={tasks} onDelete={deleteTask}/>):'No task to show'}
     </div>
     
   );
